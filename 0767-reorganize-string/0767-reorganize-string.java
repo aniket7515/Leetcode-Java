a1ea45fc0ae75176586c190c35ebfea8b06923b6
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2022-12-26 11:19:02 +0530

    Time: 5 ms (58.10%), Space: 40.4 MB (86.11%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String reorganizeString(String s) {
+        HashMap<Character,Integer> map= new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+        }
+        PriorityQueue<Character> pq= new PriorityQueue<>((a,b)-> map.get(b)-map.get(a));
+        pq.addAll(map.keySet());
+        StringBuilder sb= new StringBuilder();
+        while(pq.size()>1){
+            char first=pq.poll();
+            char second=pq.poll();
+            sb.append(first);
+            sb.append(second);
+            map.put(first,map.get(first)-1);
+            map.put(second,map.get(second)-1);
+            
+            if(map.get(first)>0) pq.offer(first);
+            if(map.get(second)>0) pq.offer(second);
+            
+        }
+        if(!pq.isEmpty()){
+            if(map.get(pq.peek())>1){
+                return "";
+            }else{
+                sb.append(pq.poll());
+            }
+        }
+        return sb.toString();
+        
+        
+    }
+    
+}
