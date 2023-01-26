77859bad1a2054eedab08b6ba526e3e41d5054f6
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2023-01-26 16:51:55 +0530

    Time: 1 ms (41.76%), Space: 43 MB (6.49%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        
+        int[][] dp= new int[m][n];
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                dp[i][j]=-1;
+            }
+        }
+        
+        if(obstacleGrid[0][0]==1) return 0;
+        
+        return func(m-1,n-1,obstacleGrid, dp);
+        
+    }
+    public int func(int i, int j, int[][] obstacleGrid,int[][] dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0 || obstacleGrid[i][j]==1) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int up=func(i,j-1,obstacleGrid,dp);
+        int left=func(i-1,j,obstacleGrid,dp);
+        
+        return dp[i][j]=up+left;
+        
+    }
+}
