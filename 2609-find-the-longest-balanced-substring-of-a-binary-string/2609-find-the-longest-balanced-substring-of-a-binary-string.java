622374df05d6d62426d089e242102ff9e24aeb8e
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2023-04-02 10:48:37 +0530

    Time: 6 ms (28.57%), Space: 42.6 MB (14.29%) - LeetHub

--- 2609-find-the-longest-balanced-substring-of-a-binary-string/2609-find-the-longest-balanced-substring-of-a-binary-string.java
+++ 2609-find-the-longest-balanced-substring-of-a-binary-string/2609-find-the-longest-balanced-substring-of-a-binary-string.java
@@ -3,7 +3,7 @@ class Solution {
         int ans=0;
         String temp="01";
         while(temp.length()<=s.length()){
-            if(s.contains(temp)) ans=temp.length();
+            if(s.indexOf(temp)>=0 && s.indexOf(temp)<s.length()) ans=temp.length();
             temp="0"+temp+"1";
         }
         return ans;
