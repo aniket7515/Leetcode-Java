fff04c1a12513a4e9d42010e039960a6e0e5e784
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2023-06-08 18:39:50 +0530

    Time: 2 ms (21.35%), Space: 44.4 MB (46.34%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,7 +1,7 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int[]  dummy1= new int[matrix.length];
-        int[]  dummy2= new int[matrix[0].length];
+        int[] dummy1= new int[matrix.length];
+        int[] dummy2= new int[matrix[0].length];
         
         Arrays.fill(dummy1,-1);
         Arrays.fill(dummy2,-1);
@@ -14,12 +14,12 @@ class Solution {
                 }
             }
         }
-           
         for(int i=0;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(dummy1[i]==0 || dummy2[j]==0) matrix[i][j]=0;
             }
         }
+        
         
       
     }
