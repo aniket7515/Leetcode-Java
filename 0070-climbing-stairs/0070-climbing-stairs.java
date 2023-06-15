604513ed7af6127d0f8ae6eb9e3b3b50f47499ac
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2023-06-15 19:33:01 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (97.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -3,15 +3,15 @@ class Solution {
         int[] dp= new int[n+1];
         Arrays.fill(dp,-1);
         return func(n,dp);
-        
-        
     }
-    public int func(int n,int[] dp){
-        if(n==0) return 0;
-        if(n==1) return 1;
-        if(n==2) return 2;
-        if(dp[n]!=-1) return dp[n];
-        dp[n]=func(n-1,dp)+func(n-2,dp);
-        return dp[n];
+    public int func(int ind, int[] dp){
+        if(ind==0) return 1;
+        if(ind<0) return 0;
+        if(dp[ind]!=-1) return dp[ind];
+        int one= func(ind-1,dp);
+        int two= func(ind-2,dp);
+        
+        return dp[ind]= one+two;
     }
+    
 }
