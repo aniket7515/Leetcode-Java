f44bb5f5a25fc4e1a4170bbd80eb891fff814bc0
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2023-06-15 23:10:44 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (69.88%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,31 +1,24 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m= obstacleGrid.length;
+        int n= obstacleGrid[0].length;
+        int[][] dp= new int[obstacleGrid.length][obstacleGrid[0].length];
+        for(int[] d:dp) Arrays.fill(d,-1);
         
-        int m=obstacleGrid.length;
-        int n=obstacleGrid[0].length;
+        return func(obstacleGrid,m-1,n-1,dp);
         
-        int[][] dp= new int[m][n];
-         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j]=-1;
-            }
-        }
-        
-        if(obstacleGrid[0][0]==1) return 0;
-        
-        return func(m-1,n-1,obstacleGrid, dp);
         
     }
-    public int func(int i, int j, int[][] obstacleGrid,int[][] dp){
+    public int func(int[][] arr, int i , int j, int[][] dp){
+        if(i>=0 && j>=0 && arr[i][j]==1) return 0;
+        if(i<0 || j<0) return 0;
+        
         if(i==0 && j==0) return 1;
-        if(i<0 || j<0 || obstacleGrid[i][j]==1) return 0;
-        
         if(dp[i][j]!=-1) return dp[i][j];
+        int left= func(arr,i-1,j,dp);
+        int up= func(arr,i,j-1,dp);
         
-        int up=func(i,j-1,obstacleGrid,dp);
-        int left=func(i-1,j,obstacleGrid,dp);
-        
-        return dp[i][j]=up+left;
-        
+        return dp[i][j]= up+left;
     }
+   
 }
