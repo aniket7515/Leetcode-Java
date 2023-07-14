057bdd742d30708a11f241aedefa4e6200fbb77f
Aniket Dhokane <80635510+aniket7515@users.noreply.github.com>
2023-07-14 21:29:35 +0530

    Time: 404 ms (11.18%), Space: 43.9 MB (85.34%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,31 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n= nums.length;
-        int recheable=0;
-        for(int i=0;i<n;i++){
-            if(recheable<i) return false;
-            recheable=Math.max(recheable,i+nums[i]);
-        }
-        return true;
+        // int n= nums.length;
+        // int recheable=0;
+        // for(int i=0;i<n;i++){
+        //     if(recheable<i) return false;
+        //     recheable=Math.max(recheable,i+nums[i]);
+        // }
+        // return true;
+        int[] dp= new int[nums.length];
+        Arrays.fill(dp,-1);
+        return func(nums,0,dp);
 
       
+    }
+    public boolean func(int[] nums, int ind, int[] dp){
+        if(ind>=nums.length-1) return true;
+        if(nums[ind]==0) return false;
+       if(dp[ind]!=-1) return dp[ind]==1;
+        int a=nums[ind];
+        for(int i=1;i<=a;i++){
+            if(func(nums,ind+i,dp)){
+                dp[ind]=1;
+                return true;
+            }
+        }
+        dp[ind]=0;
+        return false;
+        
     }
 }
